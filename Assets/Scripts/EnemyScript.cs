0062ae163d6e7dcb71b7c6ca66ead7d0f472e7d1
Brian Egana <brianm.egana@gmail.com>
2018-01-27 10:26:07 -0800

    enemy script now chases player if within distance of detection

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class EnemyScript : MonoBehaviour {
 
 	public float enemySpeed;
-	public float detectDistance = 1f;
+	public float detectDistance = 3f;
 	public GameObject player;
 
 	// Use this for initialization
@@ -18,11 +18,16 @@ public class EnemyScript : MonoBehaviour {
 		Vector2 selfPosition = transform.position;
 		Vector2 playerPosition = player.transform.position;
 
-		RaycastHit2D hit = Physics2D.Raycast(selfPosition, playerPosition - selfPosition);
+		RaycastHit2D hit = Physics2D.Raycast(selfPosition,
+			playerPosition - selfPosition);
 
 		if (hit.collider != null && hit.collider.gameObject.tag == "Player") {
-			transform.position = Vector2.MoveTowards(transform.position,
-				player.transform.position, enemySpeed);
+			float enemyToPlayerDist =
+				Mathf.Abs(Vector2.Distance(selfPosition, playerPosition));
+			if (enemyToPlayerDist < detectDistance) {
+				transform.position = Vector2.MoveTowards(transform.position,
+					player.transform.position, enemySpeed);
+			}
 		}
 	}
 }
