38f937134d298d260b2409b35d2217f212cbf8dd
jthpotz <jthpotz@gmail.com>
2018-01-27 10:33:07 -0800

    added enemy patrolling

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -1,12 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EnemyScript : MonoBehaviour {
 
 	public float enemySpeed;
 	public float detectDistance = 1f;
 	public GameObject player;
+
+	public Vector2 patrolPt1;
+	public Vector2 patrolPt2;
+
+	private bool towardPt1 = true;
+	private bool patrol = true;
 
 	// Use this for initialization
 	void Start () {
@@ -23,8 +30,34 @@ public class EnemyScript : MonoBehaviour {
 		Debug.DrawLine(selfPosition, playerPosition);
 
 		if (hit.collider != null && hit.collider.gameObject.tag == "Player") {
+			patrol = false;
 			transform.position = Vector2.MoveTowards(transform.position,
 				player.transform.position, enemySpeed);
 		}
+		else{
+			patrol = true;
+		}
+
+		if((Vector2)transform.position == patrolPt1 || (Vector2)transform.position == patrolPt2){
+			towardPt1 = !towardPt1;
+		}
+
+		if(patrol){
+			if(towardPt1){
+				transform.position = Vector2.MoveTowards (transform.position, patrolPt1, enemySpeed);
+			}
+			else{
+				transform.position = Vector2.MoveTowards (transform.position, patrolPt2, enemySpeed);
+			}
+		}
 	}
+
+	void OnCollisionEnter2D(Collision2D col){
+		Debug.Log ("Enemy Collided");
+		if(col.gameObject.tag == "Controllable"){
+			towardPt1 = !towardPt1;
+			Debug.Log ("Bounce on Enemy");
+		}
+	}
+
 }
