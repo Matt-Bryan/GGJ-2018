99355f8b47aa4157b0e06f3d63a9e7e6e37efe4a
brendenjgreenlee <brendenjgreenlee@att.net>
2018-01-26 22:57:52 -0800

    Arrow aiming and flight

---
+++ Assets/Scripts/Arrow_aim.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow_aim : MonoBehaviour {
+	private Transform arrowTransform;
+	private Vector2 arrowDirection;
+	private float mouseLocX;
+	private float mouseLocY;
+	public GameObject arrowProjectile;
+	GameObject camera;
+	Quaternion rotation;
+	private static float piTimes2 = 6.28f;
+	// Use this for initialization
+	void Start () {
+		arrowTransform = GetComponent<Transform> ();
+		camera = GameObject.Find ("Main Camera");
+		//arrowTransform.rotation.Set (0.0f, 0.0f, 90.0f, 0.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		mouseLocX = Input.mousePosition.x;
+		mouseLocY = Input.mousePosition.y;
+		arrowDirection = new Vector2 (mouseLocX - Screen.width / 2, mouseLocY - Screen.height / 2);
+		rotation = Quaternion.Euler( 0, 0, (Mathf.Atan2 (arrowDirection.y, arrowDirection.x) / piTimes2 * 360) );
+		arrowTransform.rotation = rotation;
+
+		if (Input.GetButtonDown ("Fire1"))
+			Shoot ();
+		
+		
+	}
+
+	void Shoot(){
+		GameObject projectile = (GameObject)Instantiate (arrowProjectile, transform.position, transform.rotation);
+		camera.transform.SetParent (projectile.transform);
+		GetComponent<Arrow_aim> ().enabled = false;
+
+	}
+}
---
+++ Assets/Scripts/Arrow_flight.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow_flight : MonoBehaviour {
+
+	public int arrowSpeed;
+	Rigidbody2D arrowRigidbody;
+	// Use this for initialization
+	void Start () {
+		arrowRigidbody = GetComponent<Rigidbody2D> ();
+		//arrowRigidbody.AddForce( transform.up * 10.0f, ForceMode2D.Impulse);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		arrowRigidbody.velocity = transform.right * arrowSpeed;
+	}
+}
