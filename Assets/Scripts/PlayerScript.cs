c20c1b13edd4ce0c2d17e6d8d0be8f85f119b92b
Brian Egana <brianm.egana@gmail.com>
2018-01-26 21:00:06 -0800

    completed basic movement in testing movement scene

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+	public float maxSpeed = 10;
+	public float jumpSpeed = 10;
+
+	private bool isGrounded = true;
+	private Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start() {
+		rb2d = GetComponent<Rigidbody2D>();
+	}
+
+	// FixedUpdate is called once per frame
+	void FixedUpdate() {
+		float x = Input.GetAxis("Horizontal");
+
+		rb2d.velocity = new Vector2(x * maxSpeed, rb2d.velocity.y);
+		if (isGrounded && Input.GetButtonDown("Jump")) {
+			isGrounded = false;
+			rb2d.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse);
+		}
+	}
+
+	private void OnCollisionEnter2D(Collision2D collision) {
+		isGrounded = true;
+	}
+}
