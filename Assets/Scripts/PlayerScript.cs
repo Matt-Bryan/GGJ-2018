469e48a29422e9d2737b9dcb89b48d0a27fee5ce
Brian Egana <brianm.egana@gmail.com>
2018-01-27 11:36:53 -0800

    added animation to player

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -10,16 +10,36 @@ public class PlayerScript : MonoBehaviour {
 	/*Jump Heights: 5 is one block high; 7 is two blocks high*/
 
 	private bool isGrounded = true;
+	private bool isFacingRight = true;
 	private Rigidbody2D rb2d;
+	private Animator anim;
 
 	// Use this for initialization
 	void Start() {
 		rb2d = GetComponent<Rigidbody2D>();
+		anim = GetComponent<Animator>();
 	}
 
 	// FixedUpdate is called once per frame
 	void FixedUpdate() {
 		float x = Input.GetAxis("Horizontal");
+
+		if (x < 0f && isFacingRight) {
+			//If we're moving right but not facing right, flip the sprite and set facingRight to true.
+			Flip();
+			isFacingRight = false;
+		}
+		else if (x > 0f && !isFacingRight) {
+			Flip();
+			isFacingRight = true;
+		}
+
+		if (x != 0) {
+			anim.Play("PlayerWalking");
+		}
+		else {
+			anim.Play("PlayerIdle");
+		}
 
 		rb2d.velocity = new Vector2(x * maxSpeed, rb2d.velocity.y);
 		if (isGrounded && Input.GetButtonDown("Jump")) {
@@ -30,5 +50,15 @@ public class PlayerScript : MonoBehaviour {
 
 	private void OnCollisionEnter2D(Collision2D collision) {
 		isGrounded = true;
+	}
+
+	void Flip() {
+		// Switch the way the player is labelled as facing
+		isFacingRight = !isFacingRight;
+
+		// Multiply the player's x local scale by -1
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
 	}
 }
