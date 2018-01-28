dc9acb4c7250d2c71739a8d299b13e09d2a9d679
jthpotz <jthpotz@gmail.com>
2018-01-27 17:38:23 -0800

    Fixed wall sticky

--- Assets/Scripts/ProjectileScript.cs
+++ Assets/Scripts/ProjectileScript.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class ProjectileScript : MonoBehaviour {
 
 	void OnTriggerEnter2D(Collider2D col){
-		if(col.gameObject.tag == "Ground"){
+		if(col.gameObject.tag == "Ground" || col.gameObject.tag == "Wall"){
 			GameObject.Destroy (gameObject);
 		}
 	}
