e34298fb70953f85633d7819dd5580a0e16b4414
brendenjgreenlee <brendenjgreenlee@att.net>
2018-01-28 13:49:59 -0800

    Created intro

---
+++ Assets/Scripts/Introduction.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Introduction : MonoBehaviour {
+
+	public float camMoveDistance;
+	public float camMoveTime;
+	private float currentTime = 0;
+	private Transform camTransform;
+	// Use this for initialization
+	void Start () {
+		camTransform = transform.Find ("Main Camera").transform;
+		camTransform.localPosition = new Vector3 (-60.0f, 0.0f, -10.0f);
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (currentTime < camMoveTime) {
+			camTransform.position += new Vector3 (camMoveDistance, 0.0f, 0.0f);
+			currentTime += Time.fixedDeltaTime;
+		} else {
+			camTransform.localPosition = new Vector3 (0.0f, 0.0f, -10.0f);
+		}
+			
+	}
+}
---
+++ Assets/Scripts/textIntro.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class textIntro : MonoBehaviour {
+
+	private float currentTime = 0.0f;
+	public float maxTime;
+	public float firstStep;
+	public float secondStep;
+	public float thirdStep;
+	public float fourthStep;
+	public float fifthStep;
+	public float sixthStep;
+	public float seventhStep;
+
+	public AudioClip gate;
+
+	private Text canvasText;
+	private AudioSource soundSource;
+
+	// Use this for initialization
+	void Start () {
+		soundSource = GetComponent<AudioSource> ();
+		canvasText = GetComponent<Text> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		currentTime += Time.deltaTime;
+		if (currentTime < firstStep) {
+			canvasText.text = ("Humanity, in their hubris, sought to defeat the old gods");
+		} else if (currentTime < secondStep) {
+			canvasText.text = ("They soon realized that their only hope was to restrain the gods");
+		} else if (currentTime < thirdStep) {
+			canvasText.text = ("the god of deceit was lured into a cave beneath a great volcano...");
+		} else if (currentTime < fourthStep) {
+			canvasText.text = ("And they trapped you here");
+		} else if (currentTime < fifthStep) {
+			canvasText.text = ("But that was a long time ago");
+		} else if (currentTime < sixthStep) {
+			canvasText.text = ("Now something has returned to your prison");
+		} else if (currentTime < seventhStep) {
+			canvasText.text = ("And they come with new hosts");
+		} else {
+			canvasText.text = ("[Left Click] Take Control. Escape.");
+			GameObject.Find ("Arrow").SetActive (true);
+		}
+		
+	}
+
+	void Update() {
+		if (Input.GetButtonDown("Fire1") && (currentTime > seventhStep) ){
+			Destroy(gameObject);
+		}
+	}
+}
