17a44fb6de41650af5a38d861f2d729f796352d9
jthpotz <jthpotz@gmail.com>
2018-01-28 16:38:03 -0800

    Exiting

---
+++ Assets/Scripts/ExitScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitScript : MonoBehaviour {
+
+	void Update(){
+		if(Input.GetKey ("Escape")){
+			Application.Quit ();
+		}
+	}
+
+	public void Quit(){
+		Application.Quit ();
+	}
+
+}
